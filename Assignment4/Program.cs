5b25b6debaddc820ff4c057d94657fe15b76d38d
Sunil Dagur <sunildagur566@gmail.com>
2023-08-24 21:47:55 +0530

    Add project files.

---
+++ Assignment4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Logger
+{
+    public static void LogMessage(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.LogMessage("Starting the application...");
+
+        // Your application logic here...
+
+        Logger.LogMessage("Application finished.");
+    }
+}
